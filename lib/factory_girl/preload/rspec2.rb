9c8fbe956446960f9f2330206c2b0e3137e9b124
Matt Campbell <matt@soupmatt.com>
2012-09-30 17:29:05 -0500

    config parameter to control if seeds are loaded

--- lib/factory_girl/preload/rspec2.rb
+++ lib/factory_girl/preload/rspec2.rb
@@ -1,9 +1,11 @@
 require 'rspec/core'
 
 RSpec.configure do |config|
+  config.add_setting :preload_db_seeds, :default => false
   config.include Factory::Preload::Helpers
   config.before(:suite) do
     Factory::Preload.clean
+    load("db/seeds.rb", true) if RSpec.configuration.preload_db_seeds
     Factory::Preload.run
   end
 
